6dd1d943e06134c4d19425a46062e93cd1f715eb
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-08-13 17:48:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,4 +11,18 @@ function mul(x,y){
 	return x*y
 }
 
-module.exports = {add,sub,mul}
+function div(x,y){
+	return x/y
+}
+
+
+function fun1(arg1,arg2){
+  return arg1+arg2
+}
+
+
+function fun2(arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8){
+	return arg1+arg3+arg4+arg2*arg7+arg3/arg3*arg6+arg7*arg8
+}
+
+module.exports = {add,sub,mul,div,fun1,fun2}
