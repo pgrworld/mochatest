2d092162ead15a3814f2911bdad1bc13c63dae28
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-08-14 12:38:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,4 +25,25 @@ function fun2(arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8){
 	return arg1+arg3+arg4+arg2*arg7+arg3/arg3*arg6+arg7*arg8
 }
 
-module.exports = {add,sub,mul,div,fun1,fun2}
+
+
+
+var value = 100;
+var obj = {};
+
+obj.fun1 = function(){
+
+	var text = obj.fun2(20)
+	if(text){
+     value++  
+	}
+   return value
+} 
+
+obj.fun2 = function(arg1){
+	return arg1
+}
+
+
+
+module.exports = {add,sub,mul,div,fun1,fun2,obj}
