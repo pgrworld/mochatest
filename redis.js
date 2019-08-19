e5dc5bda4cf38031b7cc3ae7d38de8100addd6c6
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-08-19 13:23:44 +0530

    Create redis.js

---
+++ redis.js
@@ -0,0 +1,78 @@
+var redis = require("redis");
+var client = redis.createClient();
+var mocha = require("./mochatest.js")
+
+client.on("connec", function(){
+	console.log("connected")
+});
+
+client.on('error', function (err) {
+    console.log('Something went wrong ' + err);
+});
+
+ 
+
+// client.get("key",function(err,reply){
+// 	//console.log(reply)
+// })
+
+// //.1
+// client.set("key","securifi")
+// client.get("key",function(err,res){
+// 	console.log(res)
+// })
+// //.2
+// client.del("key",function(err,res){
+// 	console.log(res)
+// });
+// //.3
+// client.exists("key",function(err,res){
+// 	console.log(res)
+// })
+// //.4
+// client.pexpire("key","2000",function(err,res){
+// 	console.log(res)
+// })
+
+// client.set("key1","embedded")
+// client.keys("key*",function(err,res){
+// 	console.log(res)
+// })
+
+
+// client.move("key",2,function(err,res){
+// 	console.log(res)
+// })
+
+
+// client.set("key2","hello india")
+// client.get("key2",function(err,res){
+// 	console.log(res)
+// })
+
+
+// exports.fun2 = function(returning){
+// client.get("key",function(err,res){
+// 	returning(res);
+//  })
+// }
+
+//**********************************Redis functions******************************************
+
+client.set("key","securifi", redis.print);
+client.set("key","securifi embedded systems");
+client.set("key1","i love my india");
+
+exports.fun3 = function(){
+	client.get("key",function(err,res){
+		exports.output=res
+		mocha.myfun4()
+	})
+};
+
+exports.fun5 = function(){
+	client.get("key1",function(err,res){
+		exports.output1=res
+		mocha.myfun6()
+	})
+};
