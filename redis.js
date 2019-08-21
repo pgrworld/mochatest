ac8eef7086658502fa832cc046ad3a11ae6abdd8
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-08-21 13:05:24 +0530

    Update redis.js

--- redis.js
+++ redis.js
@@ -1,94 +1,67 @@
 var redis = require("redis");
 var client = redis.createClient();
-var mocha = require("./mochatest.js")
+var obj = {};
+var response="value will be updated";
 
 client.on("connec", function(){
 	console.log("connected")
 });
-
 client.on('error', function (err) {
     console.log('Something went wrong ' + err);
 });
 
- 
-
-// client.get("key",function(err,reply){
-// 	//console.log(reply)
-// })
-
-// //.1
-// client.set("key","securifi")
-// client.get("key",function(err,res){
-// 	console.log(res)
-// })
-// //.2
-// client.del("key",function(err,res){
-// 	console.log(res)
-// });
-// //.3
-// client.exists("key",function(err,res){
-// 	console.log(res)
-// })
-// //.4
-// client.pexpire("key","2000",function(err,res){
-// 	console.log(res)
-// })
-
-// client.set("key1","embedded")
-// client.keys("key*",function(err,res){
-// 	console.log(res)
-// })
-
-
-// client.move("key",2,function(err,res){
-// 	console.log(res)
-// })
-
-
-// client.set("key2","hello india")
-// client.get("key2",function(err,res){
-// 	console.log(res)
-// })
-
-
-// exports.fun2 = function(returning){
-// client.get("key",function(err,res){
-// 	returning(res);
-//  })
-// }
-
-//**********************************Redis functions******************************************
-
-client.set("key","securifi", redis.print);
-client.set("key","securifi embedded systems");
-client.set("key1","i love my india");
-
-exports.fun3 = function(){
-	client.get("key",function(err,res){
-		exports.output=res
-		mocha.myfun4()
-	})
-};
-
-exports.fun5 = function(){
-	client.get("key1",function(err,res){
-		exports.output1=res
-		mocha.myfun6()
-	})
-};
 
 //.1
-client.set("key2","hyderabad");
+client.set("key1","vizag");
 
-client.get("key2",function(err,res){
- result = res
- mocha.mochafun(result)
+client.get("key1",function(err,res){
+obj.redisf1 = function(){
+response=res
+return response;
+ }
 })
 
 
-obj.redisfun2=function(result){
-   console.log(result)
-   return result
-}
+
+//.2
+client.set("key2","ammerica");
+
+client.get("key2",function(err,res){
+obj.redisf2 = function(){
+response=res;
+return response;
+ };
+});
+
+
+
+//.3
+client.set("key3","i am rahul");
+
+client.get("key3",function(err,res){
+obj.redisf3 = function(){
+response=res;
+return response;
+ };
+});
+
+
+
+//.4
+client.set("key4","final function");
+
+client.get("key4",function(err,res){
+obj.redisf4 = function(){
+response=res;
+return response;
+ };
+});
+
+
 
 module.exports = {obj}
+
+
+
+
+
