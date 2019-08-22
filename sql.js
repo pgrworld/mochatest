a5dd4b3a06782bc448f42a2d195543ca56db98b0
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-08-22 18:22:22 +0530

    Update sql.js

--- sql.js
+++ sql.js
@@ -1,12 +1,13 @@
-  var mysql = require('mysql');
+ var mysql = require('mysql');
  var sql={};
+ var obj={}
  var conc = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "1111",
   database: "MOCHATEST"
 });
- var obj={}
+
 
 
 
@@ -21,8 +22,8 @@ return response
 };
 
 //.2
-sql.function2 = function(){
-conc.query('delete name from mochaTABLE1 where name=?',["rahul"],function(err,res){
+sql.function2 = function(data1){
+conc.query('delete name from mochaTABLE1 where name=?',[data1],function(err,res){
 obj.sqlf2 = function(){
 response=JSON.stringify(res)
 return response    
@@ -32,23 +33,27 @@ return response
 
 //.3
 sql.function3 = function(){
-conc.query('select name,empID from mochaTABLE1 where name=?',["gowrav rahul"],function(err,res){
-obj.sqlf3 = function(){
-response=JSON.stringify(res)
-return response    
-  };	
- });
-};
-
-//.4
-sql.function4 = function(){
-conc.query('select name,empID from mochaTABLE1 where name=?',["ravi"],function(err,res){
-obj.sqlf4= function(){
+conc.query('update mochaTABLE1 set name="securifi" where name=?',["ravi"],function(err,res){
+obj.sqlf3= function(){
 response=JSON.stringify(res)
 return response    
  };	
 });
 };
 
+//.4
+sql.function4 = function(data1,data2){
+var sql1 = "insert into mochaTABLE1 (name,empID) values ('"+data1+"','"+data2+"')";
+conc.query(sql1,function(err,res){
+obj.sqlf4 = function(){
+response=JSON.stringify(res)
+return response 
+  };	
+ });
+};
+
 module.exports = {obj,sql}
+
+
+
 
