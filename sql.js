e1d997d4f0f902ce76c386f6c0308f113ddc4e17
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-08-22 13:19:45 +0530

    Update sql.js

--- sql.js
+++ sql.js
@@ -8,6 +8,7 @@
  var obj={}
  var response="value will be updated soon"
 
+//.1
 conc.query('select name,empID from mochaTABLE1 WHERE name=?', ["swaroop"],function(err,res){
 obj.sqlf1 = function(){
 response=JSON.stringify(res)
@@ -15,7 +16,7 @@ return response
  };
 });
 
-
+//.2
 conc.query('select name,empID from mochaTABLE1 where name=?',["rahul"],function(err,res){
 obj.sqlf2 = function(){
 response=JSON.stringify(res)
@@ -23,7 +24,28 @@ return response
  };	
 });
 
+//.3
+conc.query('select name,empID from mochaTABLE1 where name=?',["murali"],function(err,res){
+obj.sqlf3 = function(){
+response=JSON.stringify(res)
+return response    
+ };	
+});
+
+
+//.4
+conc.query('select name,empID from mochaTABLE1 where name=?',["ravi"],function(err,res){
+obj.sqlf4= function(){
+response=JSON.stringify(res)
+return response    
+ };	
+});
+
 
 module.exports = {obj}
+
+
+
+
 
 
