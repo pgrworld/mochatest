3fe4fbe933fc877f3e27254d788c7341e678edad
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-08-23 10:02:17 +0530

    Update redis.js

--- redis.js
+++ redis.js
@@ -1,62 +1,85 @@
 var redis = require("redis");
 var client = redis.createClient();
 var obj = {};
-var response="value will be updated";
+var redis= {};
 
-client.on("connec", function(){
-	console.log("connected")
-});
-client.on('error', function (err) {
-    console.log('Something went wrong ' + err);
-});
+//self invoking
+(function () {
+    client.set("key1","vizag");
+	client.set("key2","ammerica");
+	client.set("key3","i am rahul");
+	client.set("key4","final function");
+	client.set("key5","length checking");
+	client.mset("key6","msetkey1","key7","msetkey2","key8","msetkey3")
+}());
 
 
 //.1
-client.set("key1","vizag");
-
+redis.redisget1 = function(){
 client.get("key1",function(err,res){
 obj.redisf1 = function(){
-response=res
-return response;
- }
-})
-
+return res;
+  };
+ });
+};
 
 
 //.2
-client.set("key2","ammerica");
-
-client.get("key2",function(err,res){
+redis.redisget2 = function(){
+client.del("key2",function(err,res){
 obj.redisf2 = function(){
-response=res;
-return response;
+return res;
  };
 });
-
+};
 
 
 //.3
-client.set("key3","i am rahul");
-
-client.get("key3",function(err,res){
+redis.redisget3 = function(){
+client.exists("key3",function(err,res){
 obj.redisf3 = function(){
-response=res;
-return response;
+return res;
  };
 });
-
+};
 
 
 //.4
-client.set("key4","final function");
-
-client.get("key4",function(err,res){
+redis.redisget4 = function(){
+client.mget("key1","key2","key3","key4",function(err,res){
 obj.redisf4 = function(){
-response=res;
+response=res.toString()
+console.log(response)
 return response;
  };
 });
+};
+
+
+//.5
+redis.redisget5 = function(){
+client.strlen("key5",function(err,res){
+obj.redisf5 = function(){
+return res
+  };
+ });
+};
+
+
+//.6
+redis.redisget6 = function(){
+client.mget("key6","key7",function(err,res){
+obj.redisf6 = function(){
+response = res.toString()
+return response;
+  }
+ });
+};
+
+
+module.exports = {obj,redis}
 
 
 
-module.exports = {obj}
+
+
