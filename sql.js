02b029f7312b86bf42dba51c9cb303f3fc406ee1
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-08-26 09:54:33 +0530

    Update sql.js

--- sql.js
+++ sql.js
@@ -1,4 +1,4 @@
-  var mysql = require('mysql');
+   var mysql = require('mysql');
  var sql={};
  var conc = mysql.createConnection({
   host: "localhost",
@@ -6,51 +6,38 @@
   password: "1111",
   database: "MOCHATEST"
 });
- var obj={}
- var response=""
-
-
 
 //.1
 sql.function1 = function(callback){
 conc.query('select name,empID from mochaTABLE1 WHERE name=?', ["swaroop"],function(err,res){
-	callback(res)
+callback(res)
  });
-  return callback()
 };
 
 //.2
-sql.function2 = function(data1){
-conc.query('delete name from mochaTABLE1 where name=?',[data1],function(err,res){
-obj.sqlf2 = function(){
-response=JSON.stringify(res)
-return response    
-  };	
+sql.function2 = function(callback){
+conc.query('delete name from mochaTABLE1 where name=?',["rahul"],function(err,res){   
+callback(res)	
  });
 };
 
 //.3
-sql.function3 = function(){
+sql.function3 = function(callback){
 conc.query('update mochaTABLE1 set name="securifi" where name=?',["ravi"],function(err,res){
-obj.sqlf3= function(){
-response=JSON.stringify(res)
-return response    
- };	
-});
-};
-
-//.4
-sql.function4 = function(data1,data2){
-var sql1 = "insert into mochaTABLE1 (name,empID) values ('"+data1+"','"+data2+"')";
-conc.query(sql1,function(err,res){
-obj.sqlf4 = function(){
-response=JSON.stringify(res)
-return response 
-  };	
+callback(res)
  });
 };
 
-module.exports = {obj,sql}
+//.4
+sql.function4 = function(data1,data2,callback){
+var sql1 = "insert into mochaTABLE1 (name,empID) values ('"+data1+"','"+data2+"')";
+conc.query(sql1,function(err,res){
+callback(res)	
+ });
+};
+
+module.exports = {sql}
+
 
 
 
